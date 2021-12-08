38e876c41b02581bce027ba6814fc938d7f8595f
Nguyen Hoang Linh <linh072217@gmail.com>
2021-12-08 07:52:13 +0700

    update list files,add delete features, fix css

--- showFiles.php
+++ showFiles.php
@@ -1,33 +1,44 @@
 <?php
-  $currentDirectory = "./";
-  function readDirectory($parentDir,$dirTarget){
-    if($parentDir == ""){
-      $parentDir = ".";
-    }
-    if($parentDir == "././"){
-      $parentDir = ".";
-    }
-  
-    if(is_dir($parentDir."/".$dirTarget)){
-      if(preg_match("/^\.{1,2}$/",$dirTarget)!=1&&!empty($dirTarget)){
+  $currentDirectory = "uploads";
+  function readDirectory($dirTarget){
         $dir = "";
-        $dir = opendir($parentDir."/".$dirTarget);
+        $dir = opendir($dirTarget);
         echo "<div class='directory-container'>";
-        echo "<h3 class='directory' isOpen='true'>".$parentDir."/".$dirTarget."</h3>";
-        echo "<uL>";
+        echo "<h3 class='directory' isOpen='true'>".basename($dirTarget)."</h3>";
+        echo "<ul>";
+        $files = array();
         while($file = readdir($dir)){
-          readDirectory($parentDir."/".$dirTarget,$file);
+          if(filetype($dirTarget."/".$file) == "dir"){
+            $file_content = array("name"=>$file,"type"=>"dir", "path"=>$dirTarget."/".$file);
+            array_unshift($files,$file_content);
+          }
+          else{
+            $file_content = array("name"=>$file,"type"=>"f", "path"=>$dirTarget."/".$file);
+            array_push($files, $file_content);
+          }
         }
+        foreach($files as $index => $value){
+          if(preg_match("/^\.{1,2}$/",$value["name"]) != 1){
+            if($value["type"] == "dir"){
+              readDirectory($value["path"]);
+            }
+            else{
+              $path = $value["path"];
+              echo "<li class='file'>"."<a href='$path'>".$value["name"]."</a>"."<button class='btnDelFile'>delete</button>"."</li>";
+            }
+          }
+        }
+        // var_dump($files);
+        // while($file = readdir($dir)){
+        //   if(filetype($dirTarget.$file) == "dir"){
+            
+        //   }
+        //   echo "<li class='file'>"."<a href='$dirTarget'>".$dirTarget."</a>"."</li>";
+        // }
         echo "</uL>";
-        closedir($dir);
         echo "</div>";
-      }
-      
-    }
-    else{
-      echo "<li class='file'>"."<a href='$parentDir/$dirTarget'>".$dirTarget."</a>"."</li>";
+        closedir($dir);
       return;
-    }
   }
 ?>
 <!DOCTYPE html>
@@ -37,11 +48,28 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Show Files</title>
   <style>
+    .directory-container:hover{
+      cursor: pointer;
+    }
     .directory{
       color: red;
+      padding-left: 20px;
     }
     a{
       text-decoration: none;
+    }
+    .btnDelFile{
+      background-color: red;
+      outline: none;
+      border: none;
+      color: white;
+      margin-left: 5px;
+    }
+    .btnDelFile:hover{
+      opacity: 0.7;
+    }
+    .btnDelFile:active{
+      background-color: #4d82cb;
     }
   </style>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ==" crossorigin="anonymous"></script>
@@ -63,15 +91,34 @@
           }
             $(event.target).siblings("ul").slideToggle(1000);
         })
-        console.log(element);
+      })
+      $(".btnDelFile").each((index, element)=>{
+        $(element).on("click", (event)=>{
+          console.log($(event.target).prev())
+          $.ajax({
+            url : "deleteFile.php",
+            type : "POST",
+            data : {
+              path : $(event.target).prev("a").attr("href")
+            },
+            dataType : "text",
+            success : (response)=>{
+              responseObject = JSON.parse(response);
+              if(responseObject.code == 0){
+                $(event.target).parent().remove();
+              }
+            }
+          })
+        })
       })
     });
+
   </script>
 </head>
 <body>
   <h1>Directory contents</h1>
   <?php
-    readDirectory("",$currentDirectory);
+    readDirectory($currentDirectory);
   ?>
 </body>
 </html>
